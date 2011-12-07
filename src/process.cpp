737e0bc858cb741407c24fc8597bd98f3c15a271
Chase Douglas <chase.douglas@ubuntu.com>
2011-12-07 09:01:57 -0800

    Switch Process::Private to a POD struct
    Some other syntax cleanups too.

--- src/process.cpp
+++ src/process.cpp
@@ -11,32 +11,25 @@
 #include <cstdio>
 #include <stdexcept>
 
-namespace xorg {
-namespace testing {
-
-struct Process::Private {
-  Private()
-      : pid_(-1) {
-  }
-
-  pid_t pid_;
+struct xorg::testing::Process::Private {
+  pid_t pid;
 };
 
-Process::Process()
-    : d_(new Private()) {
+xorg::testing::Process::Process() : d_(new Private) {
+  d_->pid = -1;
 }
 
-Process::~Process() {
+xorg::testing::Process::~Process() {
   delete d_;
 }
 
-bool Process::Start(const std::string& program, ...) {
-  if (d_->pid_ == -1) {
-    d_->pid_ = vfork();
+bool xorg::testing::Process::Start(const std::string& program, ...) {
+  if (d_->pid == -1) {
+    d_->pid = vfork();
 
-    if (d_->pid_ == -1) {
+    if (d_->pid == -1) {
       return false;
-    } else if (d_->pid_ == 0) { /* Child */
+    } else if (d_->pid == 0) { /* Child */
       close(0);
       close(1);
       close(2);
@@ -45,49 +38,48 @@ bool Process::Start(const std::string& program, ...) {
       execlp(program.c_str(), program.c_str(), list, NULL);
       perror("Failed to run process.");
       exit(-1);
-    } else if (d_->pid_ > 0) { /* Parent */
     }
     return true;
   }
   return false;
 }
 
-int Process::Wait(int* status, int options) {
-  if (d_->pid_ == -1 || d_->pid_ == 0)
+int xorg::testing::Process::Wait(int* status, int options) {
+  if (d_->pid == -1 || d_->pid == 0)
     return false;
 
-  return waitpid(d_->pid_, status, options);
+  return waitpid(d_->pid, status, options);
 }
 
-bool Process::Terminate() {
-  if (d_->pid_ == -1) {
+bool xorg::testing::Process::Terminate() {
+  if (d_->pid == -1) {
     return false;
-  } else if (d_->pid_ == 0) {
+  } else if (d_->pid == 0) {
     /* Child */
     return false;
   } else { /* Parent */
-    if (kill(d_->pid_, SIGTERM) < 0) {
-      return (false);
+    if (kill(d_->pid, SIGTERM) < 0) {
+      return false;
     }
   }
   return true;
 }
 
-bool Process::Kill() {
-  if (d_->pid_ == -1) {
+bool xorg::testing::Process::Kill() {
+  if (d_->pid == -1) {
     return false;
-  } else if (d_->pid_ == 0) {
+  } else if (d_->pid == 0) {
     /* Child */
     return false;
   } else { /* Parent */
-    if (kill(d_->pid_, SIGKILL) < 0) {
-      return (false);
+    if (kill(d_->pid, SIGKILL) < 0) {
+      return false;
     }
   }
   return true;
 }
 
-bool Process::SetEnv(const char* name, const char* value,
+bool xorg::testing::Process::SetEnv(const char* name, const char* value,
                      Process::SetEnvBehaviour b) {
   if (name == NULL || value == NULL)
     return false;
@@ -95,15 +87,12 @@ bool Process::SetEnv(const char* name, const char* value,
   return setenv(name, value, static_cast<int>(b)) == 0;
 }
 
-const char* Process::GetEnv(const char* name) {
+const char* xorg::testing::Process::GetEnv(const char* name) {
   if (name == NULL)
     return NULL;
   return getenv(name);
 }
 
-pid_t Process::pid() const {
-  return d_->pid_;
+pid_t xorg::testing::Process::pid() const {
+  return d_->pid;
 }
-
-} // namespace testing
-} // namespace xorg
