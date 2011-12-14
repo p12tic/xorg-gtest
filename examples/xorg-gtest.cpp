42d3e9d818d4827ae001c50dd190fd477ffce593
Chase Douglas <chase.douglas@ubuntu.com>
2011-12-14 09:21:30 -0800

    Remove license header from example
    It clutters up the example and is unnecessary for such a small file.

--- examples/xorg-gtest.cpp
+++ examples/xorg-gtest.cpp
@@ -1,24 +1,3 @@
-/*****************************************************************************
- *
- * X testing environment - Google Test environment feat. dummy x server
- *
- * Copyright (C) 2011 Canonical Ltd.
- *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- ****************************************************************************/
-
 #include <xorg/gtest/test.h>
 
 using namespace xorg::testing;
