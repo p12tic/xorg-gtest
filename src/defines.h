6d0cd228d5ea5ff734f3bc1754d0dfbb93e6d1b4
Chase Douglas <chase.douglas@canonical.com>
2012-03-13 11:32:02 -0700

    Allow user to override default Xorg server binary
    This can be accomplished by compiling xorg-gtest with
    -DDEFAULT_XORG_SERVER=path/to/Xorg.
    
    Signed-off-by: Chase Douglas <chase.douglas@canonical.com>
    Reviewed-By: Christopher Halse Rogers <christopher.halse.rogers@canonical.com>

--- src/defines.h
+++ src/defines.h
@@ -2,7 +2,11 @@
 #define XORGGTEST_DEFINES
 
 #define DEFAULT_XORG_LOGFILE "/tmp/Xorg.GTest.log"
-#define DEFAULT_XORG_SERVER "Xorg"
 #define DEFAULT_DISPLAY 133
+
+/* Allow user to override default Xorg server*/
+#ifndef DEFAULT_XORG_SERVER
+#define DEFAULT_XORG_SERVER "Xorg"
+#endif
 
 #endif
