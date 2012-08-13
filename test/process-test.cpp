9263c0fc5be087c5876700dd53be710f39f2f317
Peter Hutterer <peter.hutterer@who-t.net>
2012-08-14 09:42:07 +1000

    test: add a self-check directory
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Chase Douglas <chase.douglas@canonical.com>

---
+++ test/process-test.cpp
@@ -0,0 +1,70 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <gtest/gtest.h>
+#include <xorg/gtest/xorg-gtest.h>
+
+using namespace xorg::testing;
+
+TEST(Process, StartWithNULLArg)
+{
+  SCOPED_TRACE("TESTCASE: invocation of 'ls' with no arguments");
+  Process p;
+  p.Start("ls", NULL);
+  ASSERT_GT(p.Pid(), 0);
+}
+
+TEST(Process, StartWithNULLTerminatedArg)
+{
+  SCOPED_TRACE("TESTCASE: invocation of 'ls' with NULL-terminated argument list");
+
+  Process p;
+  p.Start("ls", "-l", NULL);
+  ASSERT_GT(p.Pid(), 0);
+}
+
+TEST(Process, TerminationSuccess)
+{
+  SCOPED_TRACE("TESTCASE: invocation of 'echo -n', check for success exit status");
+
+  Process p;
+  ASSERT_EQ(p.GetState(), Process::NONE);
+
+  /* Process:Start closes stdout, so we need something that doesn't print */
+  p.Start("echo", "-n", NULL);
+  ASSERT_GT(p.Pid(), 0);
+  ASSERT_EQ(p.GetState(), Process::RUNNING);
+
+  /* ls shouldn't take longer terminate */
+  for (int i = 0; i < 100; i++) {
+    if (p.GetState() == Process::RUNNING)
+      usleep(5000);
+  }
+  ASSERT_EQ(p.GetState(), Process::FINISHED_SUCCESS);
+}
+
+TEST(Process, TerminationFailure)
+{
+  SCOPED_TRACE("TESTCASE: an invalid invocation of 'ls', check for error exit status");
+  Process p;
+  ASSERT_EQ(p.GetState(), Process::NONE);
+
+  /* Process:Start closes stdout, so ls should fail with status 2, if not,
+   * that file is unlikely to exists so we get status 1 */
+  p.Start("ls", "asqwerq.aqerqw_rqwe", NULL);
+  ASSERT_GT(p.Pid(), 0);
+  ASSERT_EQ(p.GetState(), Process::RUNNING);
+
+  /* ls shouldn't take longer than 5s to terminate */
+  for (int i = 0; i < 10; i++) {
+    if (p.GetState() == Process::RUNNING)
+      usleep(500);
+  }
+  ASSERT_EQ(p.GetState(), Process::FINISHED_FAILURE);
+}
+
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
